cf4eda93e37cf9c14326e3f3dbce01317597d103
Vytautas Smilingis <vytautas@smilingis.com>
2022-06-20 08:40:56 +0200

    Create miner.js

---
+++ entities/miner.js
@@ -0,0 +1,39 @@
+class Miner extends Entity {
+  constructor(direction = Direction.Up, isGhost = false) {
+    super(direction, isGhost);
+    this.speed = 0.5;
+    this.craftingTime = 120;
+    this.count = 0;
+    this.steps = 0;
+  }
+
+  produce() {
+    this.count++;
+    this.steps = 0;
+  }
+
+  work() {
+    if (this.steps >= this.craftingTime) {
+      this.produce();
+    }
+
+    this.steps += this.speed;
+  }
+
+  draw(x, y) {
+    if (this.isGhost) {
+      fill(40, 40, 40, 40);
+    } else {
+      fill(0);
+    }
+
+    rect(x * gridSize + 2, y * gridSize + 2, gridSize - 4, gridSize - 4);
+    fill(255);
+    rect(
+      x * gridSize + 2,
+      y * gridSize + 2,
+      ((gridSize - 4) * this.steps) / this.craftingTime,
+      2
+    );
+  }
+}
